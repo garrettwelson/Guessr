8675f49725568de8ff3038aef95320f26851c9d3
Garrett Welson <garrettwelson@gmail.com>
2019-10-07 09:32:37 -0500

    Adjusted port for deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app.js');
 
-const port = process.env.port || '8080';
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
